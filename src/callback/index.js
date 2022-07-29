f8600d6bf9314c5e8610847f20bf5e524cba6881
Fernando-Ibarra <fi94457@gmail.com>
2022-07-29 11:27:10 -0600

    callback, async, promise

---
+++ src/callback/index.js
@@ -0,0 +1,21 @@
+// CallBack
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+function calc(num1, num2, callback) {
+    return callback(num1, num2)
+};
+
+console.log(calc(2, 2, sum));
+
+// setTimeOut
+setTimeout(function () {
+    console.log('HOLA JAVASCRIPT');
+}, 2000)
+
+function greeting(name) {
+    console.log(`Hola ${name}`);
+};
+
+setTimeout(greeting, 2000, 'Fernando');
